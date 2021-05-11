db974b9f5dd1e1b33afa9addbf82000de66716fc
Ankur Boyed <anks5415@gmail.com>
2021-05-11 15:07:33 -0400

    dynamically add events

---
+++ events.js
@@ -0,0 +1,46 @@
+const events = [
+  {
+    name: "April Break Robotics Camp!",
+    date: "April 12th-17th, 2021",
+    description:
+      "A Robotics Camp held online where participants learned how to make their own electrical circuits on TinkerCAD.",
+    pathToFile: "robotics-camp.html",
+  },
+  {
+    name: "Boat Design Competition",
+    date: "February 21st - 28th, 2021",
+    description:
+      "A Robotics Camp held online where participants learned how to make their own electrical circuits on TinkerCAD.",
+    pathToFile: "boat-design-competition.html",
+  },
+  {
+    name: "Christmas CAD Event",
+    date: "December 5th - 24th, 2020",
+    description:
+      "Students designed and 3D modelled an ornament of their own design. Participants could get their design 3D printed and given to them to keep if they wanted.",
+    pathToFile: "cad-competition.html",
+  },
+  {
+    name: "Innovire Classes",
+    date: "October 2020 - December 2020",
+    description:
+      "Classes about CAD, Arduino, and aviation were all taught by Innovire members well-versed in the area.",
+    pathToFile: "classes.html",
+  },
+  {
+    name: "2019 Innovire Summit Conference",
+    date: "June 2019",
+    description:
+      "350+ students learned about exponential technologies! The conference was hosted at the University of Toronto - Mississauga Campus.",
+    pathToFile: "conference-2019.html",
+  },
+  {
+    name: "2018 Innovire Summit",
+    date: "June 21, 2018",
+    description:
+      "Innovire's first ever Summit! 60+ students learned about technological advancements.",
+    pathToFile: "conference-2018.html",
+  },
+];
+
+export default events;

7c6533d3353b3afdcf40923633ffdeed7bbc8430
ankan5415 <anks5415@gmail.com>
2021-07-13 12:08:29 -0400

    added 2021 conference to footer

--- footer.js
+++ footer.js
@@ -33,6 +33,9 @@ footer.innerHTML = `
                     <a href="conference-2019.html">2019 Conference</a>
                   </li>
                   <li>
+                    <a href="conference-2021.html">2021 Conference</a>
+                  </li>
+                  <li>
                     <a href="cad-competition.html">Holiday CAD</a>
                   </li>
                   <li>
@@ -83,114 +86,3 @@ footer.innerHTML = `
 </div>
 
 `;
-// document.write('<div class="footer-section">\n');
-// document.write('  <div class="container">\n');
-// document.write('    <div class="footer-top">\n');
-// document.write('      <div class="row justify-content-center">\n');
-// document.write('        <div class="col-lg-2 col-md-4">\n');
-// document.write('          <div class="footer-logo mb--50">\n');
-// document.write(
-//   '            <img src="./image/png/logo-full-white.png" alt="" />\n'
-// );
-// document.write("          </div>\n");
-// document.write("        </div>\n");
-// document.write('        <div class="col-lg-10 col-md-8">\n');
-// document.write('          <div class="row">\n');
-// document.write('            <div class="col-6 col-lg-3">\n');
-// document.write('              <div class="single-footer mb--50 mb-lg--30">\n');
-// document.write('                <h5 class="footer-title">About</h5>\n');
-// document.write('                <ul class="footer-list">\n');
-// document.write("                  <li>\n");
-// document.write(
-//   '                    <a href="about.html#mission">Our mission</a>\n'
-// );
-// document.write("                  </li>\n");
-// document.write(
-//   '                  <li><a href="about.html#team">Team Members</a></li>\n'
-// );
-// document.write("                </ul>\n");
-// document.write("              </div>\n");
-// document.write("            </div>\n");
-// document.write('            <div class="col-6 col-lg-3">\n');
-// document.write('              <div class="single-footer mb--50 mb-lg--30">\n');
-// document.write('                <h5 class="footer-title">Events</h5>\n');
-// document.write('                <ul class="footer-list">\n');
-// document.write("                  <li>\n");
-// document.write(
-//   '                    <a href="conference-2018.html">2018 Conference</a>\n'
-// );
-// document.write("                  </li>\n");
-// document.write("                  <li>\n");
-// document.write(
-//   '                    <a href="conference-2019.html">2019 Conference</a>\n'
-// );
-// document.write("                  </li>\n");
-// document.write("                  <li>\n");
-// document.write(
-//   '                    <a href="cad-competition.html">Holiday CAD</a>\n'
-// );
-// document.write("                  </li>\n");
-// document.write("                  <li>\n");
-// document.write(
-//   '                    <a href="boat-design-competition.html">Boat Design</a>\n'
-// );
-// document.write("                  </li>\n");
-// document.write("                  <li>\n");
-// document.write(
-//   '                    <a href="thunkable-workshop.html">Thunkable Workshop</a>\n'
-// );
-// document.write("                  </li>\n");
-// document.write("                </ul>\n");
-// document.write("              </div>\n");
-// document.write("            </div>\n");
-// document.write('            <div class="col-6 col-lg-3">\n');
-// document.write('              <div class="single-footer mb--50 mb-lg--30">\n');
-// document.write('                <h5 class="footer-title">Get Involved</h5>\n');
-// document.write('                <ul class="footer-list">\n');
-// document.write(
-//   '                  <li><a href="get-involved.html">Participate</a></li>\n'
-// );
-// document.write(
-//   '                  <li><a href="sponsorship.html">Sponsor</a></li>\n'
-// );
-// document.write(
-//   '                  <li><a href="join-team.html">Join Us</a></li>\n'
-// );
-// document.write("                </ul>\n");
-// document.write("              </div>\n");
-// document.write("            </div>\n");
-// document.write('            <div class="col-6 col-lg-3">\n');
-// document.write('              <div class="single-footer mb--50 mb-lg--30">\n');
-// document.write('                <h5 class="footer-title">Other</h5>\n');
-// document.write('                <ul class="footer-list">\n');
-// document.write(
-//   '                  <li><a href="contact.html">Contact</a></li>\n'
-// );
-// document.write(
-//   '                  <li><a href="blog-regular.html">Articles </a></li>\n'
-// );
-// document.write("                  <li>\n");
-// document.write(
-//   '                    <a href="current-event.html">Current Events</a>\n'
-// );
-// document.write("                  </li>\n");
-// document.write("                </ul>\n");
-// document.write("              </div>\n");
-// document.write("            </div>\n");
-// document.write("          </div>\n");
-// document.write("        </div>\n");
-// document.write("      </div>\n");
-// document.write("    </div>\n");
-// document.write('    <div class="copyright-area">\n');
-// document.write('      <div class="row align-items-center">\n');
-// document.write(
-//   '        <div class="col-sm-6 text-sm-left text-center mb-2 mb-sm-0">\n'
-// );
-// document.write('          <p class="copyright-text">\n');
-// document.write("            © 2021 Innovire, All Rights Reserved\n");
-// document.write("          </p>\n");
-// document.write("        </div>\n");
-// document.write("      </div>\n");
-// document.write("    </div>\n");
-// document.write("  </div>\n");
-// document.write("</div>\n");
